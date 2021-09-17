d654c0038274b06c677a1348d0658ff8e61f4048
hemanthyadaav <hemanthkumaryadav2001@gmail.com>
2021-09-17 15:40:12 +0530

    feat added admin route

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const port = process.env.PORT || 3030; 
+const port = process.env.PORT || 3030;
 
 const url =
   "mongodb+srv://admin:admin@fswd-p-two.fw5tq.mongodb.net/form?retryWrites=true&w=majority";
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true, 
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const formSchema = new mongoose.Schema(
@@ -21,16 +21,46 @@ const formSchema = new mongoose.Schema(
   }
 );
 
-const Form = mongoose.model("Form", formSchema); 
+const Form = mongoose.model("Form", formSchema);
 
 const formData = (bodyData) => {
-    Form({data: bodyData}).save((err) => {
-        if(err)
-        {
-            throw err; 
-        }
-    })
-}
+  Form({ data: bodyData }).save((err) => {
+    if (err) {
+      throw err;
+    }
+  });
+};
+
+//for admin to see the form details
+// var userSchema = new mongoose.Schema({
+//   name: String,
+//   phone: String,
+//   email: String,
+//   message: String,
+// });
+
+// userTable = mongoose.model("users", userSchema);
+
+var fetchData = (callback) => {
+  var userData = Form.find({});
+  
+  userData.exec(function (err, data) {
+    if (err) throw err;
+    // console.log((data));
+    return callback(data);
+  });
+};
+
+var fetchTheData = (req, res) => {
+  fetchData(function (data) {
+    res.render("user-table", { userData: data });
+  });
+};
+
+var router = express.Router();
+router.get('/admin',fetchTheData);
+
+app.use("/", router); 
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -45,8 +75,12 @@ app.get("/contact.html", (req, res) => {
 
 app.post("/contact.html", urlencodedParser, (req, res) => {
   console.log(req.body);
-  formData(req.body); 
+  formData(req.body);
   res.render("success.ejs");
 });
+
+// app.get("/admin.html", urlencodedParser, (req, res) => {
+//   res.render("admin.ejs");
+// });
 
 server.listen(port);
